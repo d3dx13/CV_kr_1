296cb87cab18a7097fd503f5dc043b7c3e312381
d3dx13 <zhidkov2846@gmail.com>
2021-03-22 15:45:17 +0300

    done 5

--- task_3.py
+++ task_3.py
@@ -1,1 +1,8 @@
-1
+import numpy as np
+
+
+
+
+
+
+
--- task_5.py
+++ task_5.py
@@ -1,1 +1,25 @@
-1
+import numpy as np
+
+Image = np.array([
+[1, 2, 3, 4, 5],
+[1, 2, 3, 4, 5],
+[1, 2, 3, 4, 5],
+[1, 2, 3, 4, 5],
+[1, 2, 3, 4, 5]
+])
+Image = np.random.randint(256, size=(5,5))
+
+hist,bins = np.histogram(Image.ravel(),256,[0,256])
+
+res = {}
+
+for i in range(256):
+    if hist[i] != 0:
+        res[i] = hist[i]
+print("Image =")
+print(Image)
+print()
+print("histogram =")
+print(res)
+print("else 0")
+
