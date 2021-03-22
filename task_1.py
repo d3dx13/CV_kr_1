908dd543644bd698ee23121dfb1a07aee7b97711
Eduard2793 <lansner1@yandex.ru>
2021-03-22 16:29:42 +0300

    task1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,35 @@
-1
+import webbrowser
+
+webbrowser.open("https://planetcalc.ru/8087/")
+
+'''
+a = input()
+
+A = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+alf = sorted(list(set(a)))
+
+tre = []
+for i in range(len(alf)):
+    k = 0
+    for j in range(len(a)):
+        if a[j] == alf[i]:
+            k += 1
+    tre.append([alf[i],k])
+
+tre = list(reversed(sorted(tre, key=lambda x: x[1])))
+
+#min(tre,key = lambda x: x[1])  
+
+def getMins(tree):
+    tree_c = tree.copy()
+    s1_m1 = tree_c.pop()
+    s2_m2 = tree_c.pop()
+    m = s1_m1[1] + s2_m2[1]
+    s = s1_m1[0] + s2_m2[0]
+    tree_c.append([])
+    tree_c = list(reversed(sorted(tree_c, key=lambda x: x[1])))
+'''
+    
+    
+    
